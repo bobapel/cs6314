1d3ac7c52f251f8f611f4601293c2b44d772eb12
bobapel <boalsal@yahoo.com>
2020-08-19 11:16:50 -0500

    web server with specific route to each project filr

--- main.js
+++ main.js
@@ -1,25 +1,56 @@
-const port = 3000,
-http = require("http"),
-httpStatus = require("http-status-codes"),
+const port = 3000;
+http = require("http");
+httpStatus = require("http-status-codes");
 fs = require("fs");
-const routeMap = {
-"/": "views/index.html"
+
+const sendErrorResponse = res => {
+res.writeHead(httpStatus.NOT_FOUND, {
+"Content-Type": "text/html"
+});
+res.write("<h1>File Not Found!</h1>");
+res.end();
 };
 http
 .createServer((req, res) => {
+let url = req.url;
+if (url.indexOf(".html") !== -1) {
 res.writeHead(httpStatus.OK, {
 "Content-Type": "text/html"
 });
-if (routeMap[req.url]) {
-fs.readFile(routeMap[req.url], (error, data) => {
+customReadFile(`./views${url}`, res);
+} else if (url.indexOf(".js") !== -1) {
+res.writeHead(httpStatus.OK, {
+"Content-Type": "text/javascript"
+});
+customReadFile(`./public/js${url}`, res);
+} else if (url.indexOf(".css") !== -1) {
+res.writeHead(httpStatus.OK, {
+"Content-Type": "text/css"
+});
+customReadFile(`./public/css${url}`, res);
+} else if (url.indexOf(".png") !== -1) {
+res.writeHead(httpStatus.OK, {
+"Content-Type": "image/png"
+});
+customReadFile(`./public/images${url}`, res);
+} else {
+sendErrorResponse(res);
+}
+})
+.listen(3000);
+console.log(`The server is listening on port number: ${port}`);
+const customReadFile = (file_path, res) => {
+if (fs.existsSync(file_path)) {
+fs.readFile(file_path, (error, data) => {
+if (error) {
+console.log(error);
+sendErrorResponse(res);
+return;
+}
 res.write(data);
 res.end();
 });
 } else {
-
-	res.end("<h1>Sorry, not found.</h1>");
+sendErrorResponse(res);
 }
-})
-.listen(port);
-console.log(`The server has started and is listening
-on port number: ${port}`);
+};
